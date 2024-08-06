713dc0bf201d705b131f069c35ab322759e03729
Henrik Engebretsen <wrongheaven73@gmail.com>
2024-08-06 17:08:38 +0200

    Add charmbracelet/huh

--- main.go
+++ main.go
@@ -3,65 +3,99 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/mail"
 	"os"
 	"strings"
+	"time"
 
 	_ "embed"
+
+	"github.com/charmbracelet/huh"
 )
 
-//go:embed templates/mit.md
+var (
+	licenseType string
+	authorName  string
+	authorEmail string
+)
+
+//go:embed licenses/mit.md
 var licenseMIT []byte
 
-/*
-Fatally exits if error exists.
-Please be nice.
-*/
-func benice(err error) {
+//go:embed licenses/apache.md
+var licenseApache []byte
+
+type License struct {
+	License []byte
+	Author  bool
+}
+
+var licenses map[string]License
+
+func init() {
+	licenses = make(map[string]License)
+	licenses["mit"] = License{licenseMIT, true}
+	licenses["apache"] = License{licenseApache, false}
+}
+
+func main() {
+	form := huh.NewForm(
+		huh.NewGroup(
+			huh.NewSelect[string]().
+				Title("Which license to create?").
+				Options(
+					huh.NewOption("MIT", "mit"),
+					huh.NewOption("Apache 2.0", "apache"),
+				).
+				Value(&licenseType),
+		),
+	)
+
+	if err := form.Run(); err != nil {
+		log.Fatal(err)
+	}
+
+	chosenLicense := licenses[licenseType]
+
+	content := strings.ReplaceAll(
+		string(chosenLicense.License),
+		"{{YEAR}}",
+		fmt.Sprint(time.Now().Year()),
+	)
+
+	if chosenLicense.Author {
+		form = huh.NewForm(
+			huh.NewGroup(
+				huh.NewInput().
+					Title("Author name").
+					Value(&authorName),
+				huh.NewInput().
+					Title("Author email").
+					Value(&authorEmail).
+					Validate(func(str string) error {
+						if _, err := mail.ParseAddress(str); err != nil {
+							return err
+						}
+						return nil
+					}),
+			),
+		)
+
+		if err := form.Run(); err != nil {
+			log.Fatal(err)
+		}
+
+		content = strings.ReplaceAll(
+			string(content),
+			"{{HOLDER}}",
+			fmt.Sprintf("%s <%s>", authorName, authorEmail),
+		)
+	}
+
+	err := os.WriteFile("LICENSE", []byte(content), 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
-}
 
-// func yesOrNo(licenseType *string, msg string) error {
-// 	fmt.Printf("%s (Y/n) ", msg)
-// 	var resp string
-// 	fmt.Scanln(&resp)
-// 	if resp == "" || strings.ToLower(resp) == "y" {
-// 		return nil
-// 	} else {
-// 		return errors.New("`no` selected")
-// 	}
-// }
-
-func main() {
-	const licenseType string = "mit"
-	const authorName string = "Henrik Engebretsen"
-	const authorEmail string = "wrongheaven73@gmail.com"
-
-	// 1. read the license from file
-	var content []byte
-	switch strings.ToLower(licenseType) {
-	case "mit":
-		content = licenseMIT
-	default:
-		log.Fatal("invalid license")
-	}
-
-	// 1a. replace placeholders with info
-	newContent := strings.ReplaceAll(string(content), "{{YEAR}}", "2024")
-	newContent = strings.ReplaceAll(
-		string(newContent),
-		"{{HOLDER}}",
-		fmt.Sprintf("%s <%s>", authorName, authorEmail),
-	)
-
-	// 2. save new license to CWD
-	err := os.WriteFile("LICENSE", []byte(newContent), 0666)
-	benice(err)
-
-	// fmt.Println("License created:")
-	// fmt.Println("")
-	// fmt.Println(newContent)
-
-	fmt.Printf("License created:\n\n%s", newContent)
+	fmt.Println("License created")
 }
